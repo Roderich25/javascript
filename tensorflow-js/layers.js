f6e4a16dcf9e722156d46b4b950047037e987005
Roderich25 <rodrigoavilasolis@gmail.com>
2020-05-26 20:21:31 -0500

    adding layers.js script

---
+++ tensorflow-js/layers.js
@@ -0,0 +1,15 @@
+console.log("Layers");
+
+// Sequential model
+const model = tf.sequential();
+
+model.add(
+  tf.layers.simpleRNN({
+    //only needed in the first layer
+    inputShape: [20, 4],
+    // number of units or neurons
+    units: 20,
+    // weight
+    recurrentInitializer: "GlorotNormal",
+  })
+);
