17a69b126eae4d504d54cafce34193c61b01a553
Roderich25 <rodrigoavilasolis@gmail.com>
2020-05-26 20:13:18 -0500

    adding model.js script

--- tensorflow-js/model.js
+++ tensorflow-js/model.js
@@ -1,2 +1,17 @@
 // Define model
 console.log("Hello World!");
+
+function simpleAdd(input1, input2) {
+  // free GPU memory once the function returns
+  return tf.tidy(() => {
+    const x1 = input1;
+    const x2 = input2;
+    const y = x1.add(x2);
+    return y;
+  });
+}
+
+const dataX1 = tf.tensor1d([1, 2, 3, 4]);
+const dataX2 = tf.tensor1d([5, 6, 7, 8]);
+const result = simpleAdd(dataX1, dataX2);
+result.print();
